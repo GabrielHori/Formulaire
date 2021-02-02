a9aec6eb78285174acb0c2f17a491722d407d8bb
Gabrielhori <lemetaisgabriel@gmail.com>
2021-02-02 10:12:32 +0100

    color change

--- js/main.js
+++ js/main.js
@@ -22,3 +22,18 @@ function unSelectAll() {
 }
 
 document.querySelector(".tab-link.active").click();
+
+
+// BUTTON COLOR BACKGROUND
+
+const button = document.querySelector('.colorbtn');
+const h2E1 = document.querySelector('h2');
+const bgE1 = document.querySelector('body');
+
+button.addEventListener('click',() =>{
+  let color = '#';
+  color += random =Math.random().toString(16).slice(2,8);
+  bgE1.style.backgroundColor = color;
+  h2E1.innerText = color;
+  console.log(color)
+})
